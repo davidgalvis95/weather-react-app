f55071a68ece5d68e0d1388d9c7c5de162c4b7f1
David Galvis <ivan.galvis@project44.com>
2021-06-09 20:10:38 -0500

    adding current weather detailed

--- src/components/CurrentWeather/CurrentWeatherDetailed.js
+++ src/components/CurrentWeather/CurrentWeatherDetailed.js
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from 'react';
+//TODO Centralize axios
+import axios from "axios";
 import classes from "./CurrentWeather.module.css";
 import Card from "../../hoc/Card/Card";
+import SunnyWithClouds from "../Icons/weather/SunnyWithClouds";
 
 //This object will be passed through props
 export const mainWeather = {
@@ -20,49 +23,64 @@ const CurrentWeatherDetailed = (props) => {
     const [weatherDetails, setWeatherDetails] = useState(mainWeather);
 
     useEffect(() => {
+
         //fetch the info from the api
         console.log("Hello");
     }, [])
 
+    async function getCurrentWeather() {
+        const currentWeather = await axios
+    }
+
 
     const renderWeatherDetailsData = function () {
         const weatherDetailsArray = Object.entries(weatherDetails);
-        return weatherDetailsArray.map((entry, index) => {
-            // const {key, value} = entry;
-            console.log(index);
+        const weatherRowDetails = weatherDetailsArray.map((entry, index) => {
             return (
-                <tr key={index}>
+                <tr key={index + 1}>
                     <td>{entry[0]}</td>
                     <td>{entry[1]}</td>
                 </tr>
             )
         })
+
+        const medianIndex = ((weatherDetailsArray.length / 2) % 2 === 0 ? (weatherDetailsArray.length / 2) + 0.5 : (weatherDetailsArray.length / 2));
+        const firstWeatherDetailsHalf = weatherRowDetails.filter(row => row.key <= medianIndex );
+        const secondWeatherDetailsHalf = weatherRowDetails.filter(row => row.key >= medianIndex );
+
+        return (
+            <div className={classes.weatherSections}>
+                <table className={`${classes.detailsTable} ${classes.detailsTable2}`}>{firstWeatherDetailsHalf}</table>
+                <table className={`${classes.detailsTable} ${classes.detailsTable2}`}>{secondWeatherDetailsHalf}</table>
+            </div>
+        );
     }
     return (
         <section className={classes.weather}>
             <Card>
-                <ul>
-                    <li>current time resume</li>
-                    <li>22:33</li>
+                <ul className={classes.list}>
+                    <li className={classes.title}>Current Weather Details</li>
+                    <li className={classes.time}>22:33</li>
                 </ul>
-                <div>
+                <div className={`${classes.weatherSections}`}>
                     <div>
-                        <div>icon</div>
-                        <div>message</div>
+                        <div>
+                            {/*TODO think how to make this to work dynamically*/}
+                            <SunnyWithClouds className={classes.iconClass}/>
+                        </div>
+                        <p>Sunny with few clouds</p>
                     </div>
-                    <div>Real feel</div>
-                </div>
-                <div className={classes.weatherSections}>
-                    <div>
-                        <div>icon</div>
-                        <div>message</div>
+                    <div className={`${classes.temperature} ${classes.temperatureDetailed}`}>20°C</div>
+                    <div className={classes.realFeel}>
+                        <h5>Real Feel</h5>
+                        <p>18°C</p>
                     </div>
-                    <div>temperature</div>
-                    <div>details</div>
                 </div>
-                <table>
-                    {renderWeatherDetailsData()}
-                </table>
+                <div className={`${classes.title} ${classes.subtitle}`}>Details</div>
+                {renderWeatherDetailsData()}
+                <div className={classes.buttonContainer}>
+                    <button>Go back →</button>
+                </div>
             </Card>
             {/*<Card>Something</Card>*/}
         </section>
