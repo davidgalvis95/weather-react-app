f63d59f9a34f82ce62c8ef04d6a1fde88c6feee7
David Galvis <>
2021-07-30 19:02:54 -0500

    fixing issue

--- src/components/CurrentWeather/Search/Search.js
+++ src/components/CurrentWeather/Search/Search.js
@@ -1,35 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import classes from "./Search.module.css";
 import { CurrentWeatherContext } from "../../../context/current-weather-context";
 import { useContext } from "react";
 
 const Search = () => {
-  const { getWeatherFunction, isLoading, queryFromMainToggleHandler, data } =
-    useContext(CurrentWeatherContext);
+
+  const {
+    getWeatherFunction,
+    isLoading,
+    searchBegan,
+    data,
+    initialReqHandler,
+  } = useContext(CurrentWeatherContext);
   const [citySearch, setCitySearch] = useState("");
-  const [shouldLoad, setShouldLoad] = useState(false);
-  const {getWeatherFunction, isLoading, searchBegan, data, initialReqHandler} = useContext(CurrentWeatherContext);
-  const [citySearch, setCitySearch] = useState('');
 
-    useEffect(() => {
-        if(!isLoading){
-            console.log(data)
-        }
-      }, [data])
+  useEffect(() => {
+    if (!isLoading) {
+      console.log(data);
+    }
+  }, [data]);
 
-    const handleKeyPress = (event) => {
-        if(event.key === 'Enter'){
-            initialReqHandler();
-            getWeatherFunction(event.target.value);
-        }
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      initialReqHandler();
+      getWeatherFunction(event.target.value);
     }
   };
 
   const handleChange = (event) => setCitySearch(event.target.value);
 
   return (
-    // <CurrentWeatherContext.Consumer>
-    // {({getWeatherFunction, isLoading}) => {
     <div className={classes.mainSearchDiv}>
       <div className={classes.searchBox}>
         <input
@@ -40,12 +40,10 @@ const Search = () => {
           value={citySearch}
         />
         <div className={classes.loaderWrapper}>
-          {isLoading && shouldLoad ? <div className={classes.loader} /> : null}
+          {isLoading && searchBegan ? <div className={classes.loader} /> : null}
         </div>
       </div>
     </div>
-    //     )}}
-    // </CurrentWeatherContext.Consumer>
   );
 };
 
